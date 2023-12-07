2741bbd89e90e0719260cf73150d009783c3502e
leeabd123 <90123334+leeabd123@users.noreply.github.com>
2023-12-06 20:28:26 -0600

    Update statistics.js

--- 411-Website/backend/models/statistics.js
+++ 411-Website/backend/models/statistics.js
@@ -1,4 +1,4 @@
-const db = require('../db'); // Ensure this is the promise-based pool
+const db = require('../db'); 
 
 const statisticsController = {
   totalDeathsByCategory: async (req, res) => {
